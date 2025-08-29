ebe02cd0c0a3e2a9ed3d5686f4703d4b55e2789f
Vedant Wankhade <vedantwankhade47@gmail.com>
2025-08-29 12:11:42 +0000

    change the get started but of the landign page to have this styeling imp

--- src/components/AnimatedButton.tsx
+++ src/components/AnimatedButton.tsx
@@ -11,15 +11,8 @@ type AnimatedButtonProps = {
 export function AnimatedButton({ href, text }: AnimatedButtonProps) {
   return (
     <Link href={href} passHref>
-      <button className="animated-button">
-        <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" />
-        </svg>
-        <span className="text">{text}</span>
-        <span className="circle" />
-        <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" />
-        </svg>
+      <button className="neumorphic-button">
+        {text}
       </button>
     </Link>
   );
